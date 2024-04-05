ca1267b29d52e11320e06a1be5deef8d80f0c231
Shlok Chavan <shlok@pentaknot.com>
2024-04-05 22:59:44 +0530

    GraphQL permission updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,4 +14,16 @@ export default ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
